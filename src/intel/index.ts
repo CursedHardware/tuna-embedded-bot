b32f891ecafc5401f74e7a97474a644abe94a9d6
Septs <github@septs.pw>
2022-04-01 20:04:23 +0800

    fix

--- src/intel/index.ts
+++ src/intel/index.ts
@@ -7,7 +7,21 @@ export const bot = new Composer()
 bot.command('/ark', async (ctx) => {
   const query = getKeyword(ctx.message)
   const results = await search(query)
-  const links = results.map((link) => `<a href="https://ark.intel.com/${link.prodUrl}">${link.label}</a>`)
+  results.sort((a, b) => a.label.localeCompare(b.label, 'en-US', { numeric: true }))
+  const links = results.map(({ prodUrl, label }) => {
+    label = label.replace(/[\xAE\u2122]/g, '')
+    const url = new URL(prodUrl, 'https://ark.intel.com')
+    if (url.pathname.endsWith('search.html')) {
+      url.searchParams.set('q', label)
+    }
+    label = label
+      .replace(/^Intel \S+ Processor/g, '')
+      .replace(/^Intel \S+ (\S+) Processor/g, '$1')
+      .replace(/^Intel UHD Graphics (\d+)/, 'UHD$1')
+      .replace(/\s+/g, ' ')
+      .trim()
+    return `<a href="${url.toString()}">${label}</a>`
+  })
   await ctx.reply(links.join('\n'), {
     parse_mode: 'HTML',
     reply_to_message_id: ctx.message.message_id,
